cf63d37d775d89add12d2c69d6509e2a367473e7
pushpalata06 <pushpalata.marade@addonfactory.in>
2015-02-25 09:32:20 +0000

    add input validation for create note domain

--- app/Domain/Note.php
+++ app/Domain/Note.php
@@ -4,48 +4,64 @@ namespace Notes\Domain;
 use Notes\Mapper\Note as NoteMapper;
 use Notes\Model\Note as NoteModel;
 use Notes\Config\Config as Configuration;
+use Notes\validator\InputValidator as InputValidator;
 
 class Note
 {
-	public function create($input)
-	{
-		$noteMapper 	= 	new NoteMapper();
-		$noteModel 		=	 new NoteModel($input);
-		$resultset 		= 	$noteMapper->create($noteModel);
-		
-		$noteModel->id 	= 	$resultset->id;
-		$noteModel->userId 	= 	$resultset->userId;
-		$noteModel->title 	= 	$resultset->title;
-		$noteModel->body 	=	$resultset->body;
-		return $noteModel;
-	}
-
-	public function delete($input)
-	{
-		$noteMapper 	= 	new NoteMapper();
-		$noteModel 		=	 new NoteModel($input);
-		$resultset 		= 	$noteMapper->delete($noteModel);
-		$noteModel->isDeleted = $resultset->isDeleted;
-		return $noteModel;
-
-	}
-
-	public function update($input)
-	{
-		$noteMapper 	= 	new NoteMapper();
-		$noteModel 		=	 new NoteModel($input);
-		$resultset 		= 	$noteMapper->update($noteModel);
-		return $resultset;
-	}
-
-	public function read($input)
-	{
-		$noteMapper 	= 	new NoteMapper();
-		$noteModel 		=	 new NoteModel($input);
-		$resultset 		= 	$noteMapper->read($noteModel);
-		$noteModel->id = $resultset->id;
-		$noteModel->title = $resultset->title;
-		$noteModel->body = $resultset->body;
-		return $noteModel;
-	}
+    public function __construct()
+    {
+        $this->validator = new InputValidator();
+    }
+    
+    public function create($input)
+    {
+        if ($this->validator->notNull($input['userId']) && $this->validator->validNumber($input['userId'])
+        	&& $this->validator->notNull($input['title']) && $this->validator->notNull($input['body'])) {
+            $noteMapper = new NoteMapper();
+            $noteModel  = new NoteModel($input);
+            $resultset  = $noteMapper->create($noteModel);
+            
+            $noteModel->id     = $resultset->id;
+            $noteModel->userId = $resultset->userId;
+            $noteModel->title  = $resultset->title;
+            $noteModel->body   = $resultset->body;
+            return $noteModel;
+        }
+    }
+    
+    public function delete($input)
+    {
+        if ($this->validator->notNull($input['id']) && $this->validator->validNumber($input['id'])
+        	&& $this->validator->notNull($input['isDeleted']) && $this->validator->validNumber($input['isDeleted'])) {
+            $noteMapper           = new NoteMapper();
+            $noteModel            = new NoteModel($input);
+            $resultset            = $noteMapper->delete($noteModel);
+            $noteModel->isDeleted = $resultset->isDeleted;
+            return $noteModel;
+        }
+    }
+    
+    public function update($input)
+    {
+        if ($this->validator->notNull($input['id']) && $this->validator->validNumber($input['id'])
+        	&& $this->validator->notNull($input['title']) && $this->validator->notNull($input['body'])) {
+            $noteMapper = new NoteMapper();
+            $noteModel  = new NoteModel($input);
+            $resultset  = $noteMapper->update($noteModel);
+            return $resultset;
+        }
+    }
+    
+    public function read($input)
+    {
+        if ($this->validator->notNull($input['id']) && $this->validator->validNumber($input['id'])) {
+            $noteMapper       = new NoteMapper();
+            $noteModel        = new NoteModel($input);
+            $resultset        = $noteMapper->read($noteModel);
+            $noteModel->id    = $resultset->id;
+            $noteModel->title = $resultset->title;
+            $noteModel->body  = $resultset->body;
+            return $noteModel;
+        }
+    }
 }
--- tests/Domain/NoteTest.php
+++ tests/Domain/NoteTest.php
@@ -6,7 +6,7 @@ use Notes\Config\Config as Configuration;
 
 class NoteTest extends \PHPUnit_Extensions_Database_TestCase
 {
-	private $connection;
+    private $connection;
     
     public function getConnection()
     {
@@ -31,75 +31,74 @@ class NoteTest extends \PHPUnit_Extensions_Database_TestCase
     {
         return $this->createXMLDataSet(dirname(__FILE__) . '/_files/domain_note_seed.xml');
     }
-
-	public function testCanCreateNote()
-	{
-		$input      = array(
+    
+    public function testCanCreateNote()
+    {
+        $input = array(
             'userId' => 1,
             'title' => 'Exception',
             'body' => 'Creating a custom exception handler is quite simple.'
         );
-
-        $noteDomain = new NoteDomain();
+        
+        $noteDomain      = new NoteDomain();
         $actualResultSet = $noteDomain->create($input);
-
+        
         $this->assertEquals(3, $actualResultSet->id);
         $this->assertEquals(1, $actualResultSet->userId);
         $this->assertEquals('Exception', $actualResultSet->title);
         $this->assertEquals('Creating a custom exception handler is quite simple.', $actualResultSet->body);
-
+        
         $query           = "select id, userId, title, body, isDeleted from Notes";
         $actualDataSet   = $this->getConnection()->createQueryTable('Notes', $query);
         $expectedDataSet = $this->createXMLDataSet(dirname(__FILE__) . '/_files/domain_note_after_insert.xml')
         ->getTable("Notes");
         $this->assertTablesEqual($expectedDataSet, $actualDataSet);
-	}
-
-	public function testCanDelete()
-	{
-		$input = array(
-			'id' =>2,
-			'isDeleted' => 1
-			);
-
-		$noteDomain = new NoteDomain();
-		$actualResultSet = $noteDomain->delete($input);
-
-		$this->assertEquals(1, $actualResultSet->isDeleted);
-
-	}
-
-	public function testCanUpdate()
-	{
-		 $input           = array(
+    }
+    
+    public function testCanDelete()
+    {
+        $input = array(
+            'id' => 2,
+            'isDeleted' => 1
+        );
+        
+        $noteDomain      = new NoteDomain();
+        $actualResultSet = $noteDomain->delete($input);
+        
+        $this->assertEquals(1, $actualResultSet->isDeleted);
+        
+    }
+    
+    public function testCanUpdate()
+    {
+        $input = array(
             'id' => 1,
             'title' => 'Web',
             'body' => 'PHP is a powerful tool for making dynamic Web pages.'
         );
-
-		$noteDomain = new NoteDomain();
-		$actualResultSet = $noteDomain->update($input);
-
-		$this->assertEquals("Updated Successfully", $actualResultSet);
+        
+        $noteDomain      = new NoteDomain();
+        $actualResultSet = $noteDomain->update($input);
+        
+        $this->assertEquals("Updated Successfully", $actualResultSet);
         $query           = "select id, title, body from Notes";
         $actualDataSet   = $this->getConnection()->createQueryTable('Notes', $query);
         $expectedDataSet = $this->createXMLDataSet(dirname(__FILE__) . '/_files/domain_note_after_update.xml')
         ->getTable("Notes");
         $this->assertTablesEqual($expectedDataSet, $actualDataSet);
-
-	}
-
-	public function testReadById()
-	{
-		$input           = array(
+        
+    }
+    
+    public function testReadById()
+    {
+        $input           = array(
             'id' => 1
-            );
-		$noteDomain = new NoteDomain();
-		$actualResultSet = $noteDomain->read($input);
-
-		$this->assertEquals(1, $actualResultSet->id);
-		$this->assertEquals('PHP', $actualResultSet->title);
-		$this->assertEquals('Preprocessor Hypertext', $actualResultSet->body);
-	}
+        );
+        $noteDomain      = new NoteDomain();
+        $actualResultSet = $noteDomain->read($input);
+        
+        $this->assertEquals(1, $actualResultSet->id);
+        $this->assertEquals('PHP', $actualResultSet->title);
+        $this->assertEquals('Preprocessor Hypertext', $actualResultSet->body);
+    }
 }
-
