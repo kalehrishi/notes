821099617b7c66c20deb2fe901ead4464ffe103b
amitheda <amit.heda@addonfactory.in>
2015-03-04 13:26:30 +0000

    Added NoteTagCollection and UserTagCollection tests

--- tests/Collection/UserTagCollectionTest.php
+++ tests/Collection/UserTagCollectionTest.php
@@ -1,1 +1,46 @@
+<?php
 
+namespace Notes\Collection;
+
+use Notes\Model\UserTag as UserTagModel;
+
+class UserTagCollectionTest extends \PHPUnit_Framework_TestCase
+{   
+    /**
+    * @test
+    *
+    */
+    public function it_should_be_added_multiple_object()
+    {
+         $resulset = array(
+         	'0'=>array(
+                'id' => 41,
+                'userId' => 4,
+                'tag' => 'People',
+                'isDeleted' => 0
+              ),
+         	'1'=>array(
+                'id' => 122,
+                'userId' => 71,
+                'tag' => 'Import',
+                'isDeleted' => 0
+              )
+         	);
+
+
+    	$userTagCollection = new UserTagCollection($resulset);
+
+    	$userTagCollection->rewind();
+        while($userTagCollection->hasNext()) {
+        $this->assertEquals(122, $userTagCollection->getRow(1)->getId());
+        $this->assertEquals(71, $userTagCollection->getRow(1)->getUserId());
+        $this->assertEquals('Import', $userTagCollection->getRow(1)->getTag());
+        $this->assertEquals(0, $userTagCollection->getRow(1)->getIsDeleted());
+        $this->assertEquals(41, $userTagCollection->getRow(0)->getId());
+        $this->assertEquals(4, $userTagCollection->getRow(0)->getUserId());
+        $this->assertEquals('People', $userTagCollection->getRow(0)->getTag());
+        $this->assertEquals(0, $userTagCollection->getRow(0)->getIsDeleted());
+        $userTagCollection->next();
+        }  
+    }
+}
