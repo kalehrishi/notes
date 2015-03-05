41e2c3a418e413cb1b16338f5722a394d7be001c
Vagrant User <vagrant@packer-virtualbox-iso.vagrantup.com>
2015-03-05 11:52:51 +0000

    aded test case for userfactory

--- tests/Factory/UserFactoryTest.php
+++ tests/Factory/UserFactoryTest.php
@@ -5,37 +5,25 @@ use Notes\Model\User as UserModel;
 
 use Notes\Factory\UserFactory as UserFactory;
 
-class UserTest extends \PHPUnit_Framework_TestCase
+class UserFactoryTest extends \PHPUnit_Framework_TestCase
 {
     public function testCanSetMultipleProperties()
     {
         
-        $input     = array(
+        $input       = array(
             'firstName' => 'anusha',
             'lastName' => 'hiremath',
             'email' => 'anusha@gmail.com',
             'password' => 'sfhsk1223',
             'createdOn' => '2014-10-31 20:59:59'
         );
-        $userFactory     = new UserFactory();
-        $userModel       = $userFactory->create($input);
+        $userFactory = new UserFactory();
+        $userModel   = $userFactory->create($input);
         $this->assertEquals('anusha', $userModel->getFirstName());
         $this->assertEquals('hiremath', $userModel->getLastName());
         $this->assertEquals('anusha@gmail.com', $userModel->getEmail());
         $this->assertEquals('sfhsk1223', $userModel->getPassword());
         $this->assertEquals('2014-10-31 20:59:59', $userModel->getCreatedOn());
-      
-    }
-    
-    public function testCanSetSingleProperty()
-    {
-        
-        $input     = array(
-            'id' => 1
-        );
-        $userFactory     = new UserFactory();
-        $userModel       = $userFactory->read($input);
-        $this->assertEquals(1, $userModel->getId());
         
     }
 }
