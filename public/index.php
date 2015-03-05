275c8024bed35ecb124eb9200c1cff5ba9cc4247
amitheda <amit.heda@addonfactory.in>
2015-03-05 12:36:42 +0000

    Added slim dependency in composer

--- public/index.php
+++ public/index.php
@@ -1,11 +1,3 @@
 <?php
 
-
-
 require '../vendor/autoload.php';
-
-
-$userService=new Notes\Service\User();
-
-$input=array('firstName'=>'FirstName','lastName'=>'LastName','email'=>'test@notes.com');
-print_r($userService->createUser($input));
