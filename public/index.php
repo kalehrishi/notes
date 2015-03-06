ff3a0cfed11004e50474e34dbd41e4111130fcef
amitheda <amit.heda@addonfactory.in>
2015-03-06 09:58:28 +0000

    Added rewrite rule in .htaccess

--- public/index.php
+++ public/index.php
@@ -1,3 +1,26 @@
 <?php
 
+namespace Notes\Domain;
+
 require '../vendor/autoload.php';
+
+use Notes\Model\UserTag as UserTagModel;
+
+$app = new \Slim\Slim();
+
+$app->get('/tag/:tag', function ($tag) {
+
+    $userTagModel= new UserTagModel();
+    
+    $userTagModel->setUserId(2);
+    $userTagModel->setTag($tag);
+
+    $userTagDomain= new UserTag();
+    
+    $result=$userTagDomain->create($userTagModel);
+
+    print_r($result);
+});
+
+$app->run();
+
