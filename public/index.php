92f11b6473552b153d9c51ab24dc2f4ab841f29f
amitheda <amit.heda@addonfactory.in>
2015-03-13 07:01:57 +0000

    Added login page

--- public/index.php
+++ public/index.php
@@ -6,20 +6,19 @@ use Notes\Request\Request as Request;
 use Notes\Response\Response as Response;
 use Notes\Controller\User as UserController;
 
-$application = new \Slim\Slim(array(
-    'debug' => true
-));
+
+$application = new \Slim\Slim(array('debug' => true));
 
 
-$application->get('/env', function() {
-    $env = \Slim\Environment::getInstance();
-    echo "<br/>slim.url_scheme=" . $env['slim.url_scheme'];
-    echo "<br/>REQUEST_METHOD=" . $env['REQUEST_METHOD'];
-    echo "<br/>SCRIPT_NAME=" . $env['SCRIPT_NAME'];
-    echo "<br/>PATH_INFO=" . $env['PATH_INFO'];
-    echo "<br/>SERVER_NAME=" . $env['SERVER_NAME'];
+$application->get('/env', function(){
+ $env=\Slim\Environment::getInstance();
+ echo "<br/>slim.url_scheme=".$env['slim.url_scheme'];
+ echo "<br/>REQUEST_METHOD=".$env['REQUEST_METHOD'];
+ echo "<br/>SCRIPT_NAME=".$env['SCRIPT_NAME'];
+ echo "<br/>PATH_INFO=".$env['PATH_INFO'];
+ echo "<br/>SERVER_NAME=".$env['SERVER_NAME'];    
     
-});
+}); 
 
 $application->get('/', function() {
     echo "Home Page";
@@ -27,15 +26,22 @@ $application->get('/', function() {
 
 $application->post('/register', function() {
     $paramsData = \Slim\Slim::getInstance()->request()->getBody();
-    
-    $objRequest = new Request($paramsData);
-    $request    = $objRequest->read();
-    
-    $userController = new UserController();
-    $response       = $userController->create($request);
-    
-    $objResopnse = new Response();
-    $data        = $objResopnse->toJson($response);
+      
+    $objRequest=new Request($paramsData);
+    $request=$objRequest->read();
+       
+    $userController=new UserController();
+    $response=$userController->create($request);
+       
+    $objResopnse=new Response();
+    $data=$objResopnse->to_json($response);
     echo $data;
+});
+
+$application->post('/login', function() {
+    $paramsData = \Slim\Slim::getInstance()->request()->getBody();
+   
+    $objRequest=new Request($paramsData);
+    $request=$objRequest->read();
 });
 $application->run();
