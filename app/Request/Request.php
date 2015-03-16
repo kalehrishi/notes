a1d792e4d204226688b86c4139b27af08932a438
amitheda <amit.heda@addonfactory.in>
2015-03-16 11:55:23 +0000

    Added Request class structure

---
+++ app/Request/Request.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Notes\Request;
+
+class Request
+{
+	protected $request;
+
+	public function __construct($request)
+    {
+        $this->request=$request;    
+    }
+    public function data()
+    {
+       return json_decode($this->request->getBody(),true);
+    }
+    public function version()
+    {
+       return $this->request->getScheme();
+    }
+    public function cookies()
+    {
+       return $this->request->cookies;
+    }
+    
+}
