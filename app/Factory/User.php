51aeb789766a8cf58a5b8033f3a6c88927028002
Anusha Hiremath <Anusha Hiremath>
2015-03-16 06:28:43 +0000

    added UserFactory and its testcases

---
+++ app/Factory/User.php
@@ -0,0 +1,56 @@
+<?php
+namespace Notes\Factory;
+
+use Notes\Model\User as UserModel;
+
+use Notes\Validator\InputValidator as InputValidator;
+
+use Notes\PasswordValidation\PasswordValidator as PasswordValidator;
+
+class User
+{
+    
+    public function __construct()
+    {
+        $this->validator = new InputValidator();
+        
+    }
+    public function create($input)
+    {
+        
+        $userModel = new UserModel();
+        foreach ($input as $key => $value) {
+            if ($key == 'id') {
+                if ($this->validator->notNull($input['id'])) {
+                    $userModel->setId($input['id']);
+                    
+                }
+            }
+            if ($key == 'firstName') {
+                if ($this->validator->notNull($input['firstName'])
+                    && $this->validator->validString($input['firstName'])
+                    && $this->validator->validString($input['lastName'])) {
+                    $userModel->setFirstName($input['firstName']);
+                    $userModel->setLastName($input['lastName']);
+                    $userModel->setCreatedOn($input['createdOn']);
+                                                           
+                }
+            }
+           
+            if ($key == 'email') {
+                if ($this->validator->validEmail($input['email'])
+                    && $this->validator->isValidPassword($input['password'])) {
+                    $userModel->setEmail($input['email']);
+                    $userModel->setPassword($input['password']);
+                    
+                    
+                }
+            }
+          
+                
+                    
+        }
+        return $userModel;
+        
+    }
+}
