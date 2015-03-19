97ca288a02f6a4471e4500b488dbf329cc017387
Anusha Hiremath <anusha.hiremath@addonfactory.in>
2015-03-19 13:25:36 +0000

    added UserService and UserController

---
+++ app/Controller/User.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Notes\Controller;
+
+use Notes\Service\User as UserService;
+
+class User
+{
+    public function create($request)
+    {
+        
+        $userModel = new UserModel();
+        $userModel->setFirstName($request['firstName']);
+        $userModel->setLastName($request['lastName']);
+        $userModel->setEmail($request['email']);
+        $userModel->setPassword($request['password']);
+        $userModel->setCreatedOn($request['createdOn']);
+        
+        try {
+            $userService = new UserService();
+            $response    = $userService->create($userModel);
+        } catch (\InvalidArgumentException $e) {
+            $response = $e->getMessage();
+        }
+        return $response;
+    }
+}
