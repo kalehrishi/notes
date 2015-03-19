9a9e95f27e62c332fa43b761f5bf3cbde44e0421
amitheda <amit.heda@addonfactory.in>
2015-03-19 12:20:51 +0000

    Added Response class

--- app/Response/Response.php
+++ app/Response/Response.php
@@ -4,21 +4,22 @@ namespace Notes\Response;
 
 class Response
 {
-    public function __construct($code, $status, $data)
+    private $statusCode;
+    private $message;
+    private $version;
+    private $data;
+    private $debug_data;
+    
+    public function __construct($statusCode, $message, $version, $data, $debug_data)
     {
-        if ($status = "success") {
-            $this->toJson($data);
-        } else {
-            $this->ErrorMessage();
-        }
-        
+        $this->statusCode = $statusCode;
+        $this->message    = $message;
+        $this->version    = $version;
+        $this->data       = $data;
+        $this->debug_data = $debug_data;
     }
-    public function errorMessage()
+    public function getResponse()
     {
-        
-    }
-    public function toJson()
-    {
-        
+        return json_encode(get_object_vars($this));
     }
 }
