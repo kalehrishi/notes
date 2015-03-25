e3f88148cf944f8017e1a3ee7f02a3d00efb1614
amitheda <amit.heda@addonfactory.in>
2015-03-25 06:07:25 +0000

    Added test case for toArray method in NoteTag and UserTag Model

--- tests/Model/NoteTagTest.php
+++ tests/Model/NoteTagTest.php
@@ -6,41 +6,84 @@ use Notes\Model\UserTag as UserTag;
 
 class NoteTagTest extends \PHPUnit_Framework_TestCase
 {
-    public function testCanSetAndAccessMultipleProperties()
+    /**
+     *@test
+     *
+     **/
+    public function it_should_set_multiple_properties()
     {
         
-        $input   = array(
+        $noteTagInput   = array(
             'id' => 125,
             'noteId' => 48,
             'userTagId' => 25,
             'isDeleted' => 1,
             'userTag' => 'Javascript'
         );
-        $noteTag = new NoteTag();
-        $noteTag->setId($input['id']);
-        $noteTag->setNoteId($input['noteId']);
-        $noteTag->setUserTagId($input['userTagId']);
-        $noteTag->setIsDeleted($input['isDeleted']);
+        $noteTagModel = new NoteTag();
+        $noteTagModel->setId($noteTagInput['id']);
+        $noteTagModel->setNoteId($noteTagInput['noteId']);
+        $noteTagModel->setUserTagId($noteTagInput['userTagId']);
+        $noteTagModel->setIsDeleted($noteTagInput['isDeleted']);
         
-        $userTag = new UserTag();
-        $userTag->setTag($input['userTag']);
-
-        $this->assertEquals(125, $noteTag->getId());
-        $this->assertEquals(48, $noteTag->getNoteId());
-        $this->assertEquals(25, $noteTag->getuserTagId());
-        $this->assertEquals(1, $noteTag->getIsDeleted());
+        $userTagModel = new UserTag();
+        $userTagModel->setTag($noteTagInput['userTag']);
         
-        $this->assertEquals('Javascript', $userTag->getTag());
+        $this->assertEquals(125, $noteTagModel->getId());
+        $this->assertEquals(48, $noteTagModel->getNoteId());
+        $this->assertEquals(25, $noteTagModel->getuserTagId());
+        $this->assertEquals(1, $noteTagModel->getIsDeleted());
+        
+        $this->assertEquals('Javascript', $userTagModel->getTag());
         
     }
-    public function testCanSetAndAccessSingleProperty()
+    /**
+     *@test
+     *
+     **/
+    public function it_should_set_single_property()
     {
-        $input   = array(
+        $noteTagInput   = array(
             'noteId' => 1
         );
-        $noteTag = new NoteTag();
-        $noteTag->setNoteId($input['noteId']);
+        $noteTagModel = new NoteTag();
+        $noteTagModel->setNoteId($noteTagInput['noteId']);
         
-        $this->assertEquals(1, $noteTag->getNoteId());
+        $this->assertEquals(1, $noteTagModel->getNoteId());
+    }
+    /**
+     *@test
+     *
+     **/
+    public function it_should_convert_object_into_array()
+    {
+        $noteTagInput = array(
+            'id' => 125,
+            'noteId' => 48,
+            'userTagId' => 25,
+            'isDeleted' => 1,
+            'userTag' => array(
+                'id' => 1,
+                'userId' => 2,
+                'tag' => 'People',
+                'isDeleted' => 1
+            )
+        );
+        $noteTagModel = new NoteTag();
+        $noteTagModel->setId($noteTagInput['id']);
+        $noteTagModel->setNoteId($noteTagInput['noteId']);
+        $noteTagModel->setUserTagId($noteTagInput['userTagId']);
+        $noteTagModel->setIsDeleted($noteTagInput['isDeleted']);
+        $noteTagModel->setIsDeleted($noteTagInput['isDeleted']);
+        
+        $userTagModel = new UserTag();
+        $userTagModel->setId($noteTagInput['userTag']['id']);
+        $userTagModel->setUserId($noteTagInput['userTag']['userId']);
+        $userTagModel->setTag($noteTagInput['userTag']['tag']);
+        $userTagModel->setIsDeleted($noteTagInput['userTag']['isDeleted']);
+        
+        $noteTagModel->setUserTag($userTagModel);
+        
+        $this->assertEquals($noteTagInput, $noteTagModel->toArray());
     }
 }
