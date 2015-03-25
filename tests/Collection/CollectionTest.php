68243f1a44b401dae18156d3d3e3d0ee2d622787
amitheda <amit.heda@addonfactory.in>
2015-03-25 05:29:40 +0000

    Added testCases for toArray Method in collection and in it's derived classes

--- tests/Collection/CollectionTest.php
+++ tests/Collection/CollectionTest.php
@@ -5,17 +5,17 @@ namespace Notes\Collection;
 use Notes\Model\NoteTag as NoteTagModel;
 
 class CollectionTest extends \PHPUnit_Framework_TestCase
-{   
+{
     private $collection;
-
+    
     public function setUp()
     {
         $this->collection = new Collection();
     }
     /**
-    * @test
-    *
-    **/
+     * @test
+     *
+     **/
     public function it_should_be_added_single_object()
     {
         $noteTagModel = new NoteTagModel();
@@ -34,74 +34,74 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
         }
     }
     /**
-    * @test
-    * @expectedException         InvalidArgumentException
-    * @expectedExceptionMessage  Input should be Object
-    **/
+     * @test
+     * @expectedException         InvalidArgumentException
+     * @expectedExceptionMessage  Input should be Object
+     **/
     
     public function throws_Exception_when_added_an_array()
     {
-        $array=array(
-            'test'=>'test',
-            'variables'=>3
-            );
+        $array = array(
+            'test' => 'test',
+            'variables' => 3
+        );
         $this->collection->add($array);
     }
     /**
-    * @test
-    * @expectedException         OutOfBoundsException
-    * @expectedExceptionMessage  Array index is out of bounds
-    **/
+     * @test
+     * @expectedException         OutOfBoundsException
+     * @expectedExceptionMessage  Array index is out of bounds
+     **/
     
     public function throws_Exception_while_accessing_array_out_of_bounds()
     {
-        $noteTagModel  = new NoteTagModel();
+        $noteTagModel = new NoteTagModel();
         $noteTagModel->setId(41);
         $noteTagModel->setNoteId(4);
         $noteTagModel->setUserTagId(543);
         $noteTagModel->setIsDeleted(0);
         
         $this->collection->add($noteTagModel);
-
+        
         $this->assertEquals($noteTagModel, $this->collection->getRow(1));
     }
     /**
-    * @test
-    * @expectedException         OutOfBoundsException
-    * @expectedExceptionMessage  Array index is out of bounds
-    **/
+     * @test
+     * @expectedException         OutOfBoundsException
+     * @expectedExceptionMessage  Array index is out of bounds
+     **/
     
     public function throws_Exception_while_collection_object_is_null()
     {
         $this->collection->getRow(0);
     }
     /**
-    * @test
-    *
-    **/
+     * @test
+     *
+     **/
     public function it_should_be_added_multiple_objects()
     {
-        $noteTagModel  = new NoteTagModel();
+        $noteTagModel = new NoteTagModel();
         $noteTagModel->setId(41);
         $noteTagModel->setNoteId(4);
         $noteTagModel->setUserTagId(543);
         $noteTagModel->setIsDeleted(0);
         
         $this->collection->add($noteTagModel);
-
+        
         $this->assertEquals($noteTagModel, $this->collection->getRow(0));
         
-        $noteTagModel  = new NoteTagModel();
+        $noteTagModel = new NoteTagModel();
         $noteTagModel->setId(122);
         $noteTagModel->setNoteId(71);
         $noteTagModel->setUserTagId(63);
         $noteTagModel->setIsDeleted(0);
-       
+        
         $this->collection->add($noteTagModel);
         
         $this->assertEquals($noteTagModel, $this->collection->getRow(1));
-              
-        $noteTagModel  = new NoteTagModel();
+        
+        $noteTagModel = new NoteTagModel();
         $noteTagModel->setId(12);
         $noteTagModel->setNoteId(1);
         $noteTagModel->setUserTagId(93);
@@ -109,77 +109,89 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
         
         $this->collection->add($noteTagModel);
         
-        $this->assertEquals($noteTagModel, $this->collection->getRow(2));  
-
-
+        $this->assertEquals($noteTagModel, $this->collection->getRow(2));
+        
+        
         $this->collection->rewind();
-        while($this->collection->hasNext()) {
-           $this->assertEquals(true,$this->collection->valid());
-           $this->collection->next();
-       }   
-
+        while ($this->collection->hasNext()) {
+            $this->assertEquals(true, $this->collection->valid());
+            $this->collection->next();
+        }
+        
         $this->collection->rewind();
-        while($this->collection->hasNext()) {
-        $this->assertEquals(122, $this->collection->getRow(1)->getId());
-        $this->assertEquals(71, $this->collection->getRow(1)->getNoteId());
-        $this->assertEquals(63, $this->collection->getRow(1)->getUserTagId());
-        $this->assertEquals(0, $this->collection->getRow(1)->getIsDeleted());
-        $this->assertEquals(41, $this->collection->getRow(0)->getId());
-        $this->assertEquals(4, $this->collection->getRow(0)->getNoteId());
-        $this->assertEquals(543, $this->collection->getRow(0)->getUserTagId());
-        $this->assertEquals(0, $this->collection->getRow(0)->getIsDeleted());
-        $this->assertEquals(12, $this->collection->getRow(2)->getId());
-        $this->assertEquals(1, $this->collection->getRow(2)->getNoteId());
-        $this->assertEquals(93, $this->collection->getRow(2)->getUserTagId());
-        $this->assertEquals(1, $this->collection->getRow(2)->getIsDeleted());
-        $this->collection->next();
-        }  
+        while ($this->collection->hasNext()) {
+            $this->assertEquals(122, $this->collection->getRow(1)->getId());
+            $this->assertEquals(71, $this->collection->getRow(1)->getNoteId());
+            $this->assertEquals(63, $this->collection->getRow(1)->getUserTagId());
+            $this->assertEquals(0, $this->collection->getRow(1)->getIsDeleted());
+            $this->assertEquals(41, $this->collection->getRow(0)->getId());
+            $this->assertEquals(4, $this->collection->getRow(0)->getNoteId());
+            $this->assertEquals(543, $this->collection->getRow(0)->getUserTagId());
+            $this->assertEquals(0, $this->collection->getRow(0)->getIsDeleted());
+            $this->assertEquals(12, $this->collection->getRow(2)->getId());
+            $this->assertEquals(1, $this->collection->getRow(2)->getNoteId());
+            $this->assertEquals(93, $this->collection->getRow(2)->getUserTagId());
+            $this->assertEquals(1, $this->collection->getRow(2)->getIsDeleted());
+            $this->collection->next();
+        }
     }
     /**
-    *@test
-    *
-    **/
+     *@test
+     *
+     **/
     public function it_should_convert_object_into_array()
     {
-         $noteTagModel = array(
-            '0'=>array(
-                'id' => 1,
-                'noteId' => 3,
-                'userTagId' => 1,
-                'isDeleted' => 0,
-                'userTag' => array(
-                    'id' => 1,
-                    'userId' => 1,
-                    'tag' => 'OOP PHP',
-                    'isDeleted' => 0)
-              ),
-            '1'=>array(
-                'id' => 2,
-                'noteId' => 3,
-                'userTagId' => 3,
-                'isDeleted' => 0,
-                'userTag' => array(
-                    'id' => 2,
-                    'userId' => 1,
-                    'tag' => 'First Tag',
-                    'isDeleted' => 0)
-              ),
-            '2'=>array(
-                'id' => 2,
-                'noteId' => 3,
-                'userTagId' => 4,
-                'isDeleted' => 0,
-                'userTag' => array(
-                    'id' => 3,
-                    'userId' => 1,
-                    'tag' => 'Second Tag',
-                    'isDeleted' => 0)
-              )
-            );
-        $noteTagCollection = new Collection($noteTagModel);
-        print_r($noteTagCollection->toArray());
+        $noteTagModel = new NoteTagModel();
+        $noteTagModel->setId(41);
+        $noteTagModel->setNoteId(4);
+        $noteTagModel->setUserTagId(543);
+        $noteTagModel->setIsDeleted(0);
         
+        $this->collection->add($noteTagModel);
         
-        $this->assertEquals($noteTagModel,$noteTagCollection->toArray());
+        $noteTagModel = new NoteTagModel();
+        $noteTagModel->setId(122);
+        $noteTagModel->setNoteId(71);
+        $noteTagModel->setUserTagId(63);
+        $noteTagModel->setIsDeleted(0);
+        
+        $this->collection->add($noteTagModel);
+        
+        $noteTagModel = new NoteTagModel();
+        $noteTagModel->setId(12);
+        $noteTagModel->setNoteId(1);
+        $noteTagModel->setUserTagId(93);
+        $noteTagModel->setIsDeleted(1);
+        
+        $this->collection->add($noteTagModel);
+        
+        $expectedArray = array(
+            "0" => array(
+                "id" => 41,
+                "noteId" => 4,
+                "userTagId" => 543,
+                "isDeleted" => 0,
+                "userTag" => null
+            ),
+            
+            "1" => array(
+                "id" => 122,
+                "noteId" => 71,
+                "userTagId" => 63,
+                "isDeleted" => 0,
+                "userTag" => null
+            ),
+            
+            "2" => array(
+                "id" => 12,
+                "noteId" => 1,
+                "userTagId" => 93,
+                "isDeleted" => 1,
+                "userTag" => null
+            )
+            
+        );
+        
+        $this->assertEquals($expectedArray, $this->collection->toArray());
     }
 }
