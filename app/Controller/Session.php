97b512c02ae4466a62805a622d72fad0fc8048e5
gauribhapkar <gauribhapkar@github.com>
2015-03-26 08:46:36 +0000

    add Session Controller using Request class Object

---
+++ app/Controller/Session.php
@@ -0,0 +1,50 @@
+<?php
+namespace Notes\Controller;
+
+use Notes\Model\User as UserModel;
+use Notes\Model\Session as SessionModel;
+use Notes\Service\Session as SessionService;
+use Notes\Exception\ModelNotFoundException as ModelNotFoundException;
+
+class Session
+{
+    protected $request;
+    
+    public function __construct($request)
+    {
+        $this->request = $request;
+    }
+    
+    public function post()
+    {
+        $data_array = $this->request->getData();
+        $data       = $data_array['data'];
+        
+        try {
+            $sessionService = new SessionService();
+            $userModel      = $sessionService->login($data);
+        } catch (\ModelNotFoundException $e) {
+            $response = $e->getMessage();
+        }
+        return $userModel;
+    }
+    
+    public function delete()
+    {
+        $data_array = $this->request->getData();
+        $data       = $data_array['data'];
+        
+        $sessionModel = new sessionModel();
+        $sessionModel->setAuthToken($data['authToken']);
+        $sessionModel->setUserId($data['userId']);
+        try {
+            $sessionService   = new SessionService();
+            $sessionModelRead = $sessionService->isValid($sessionModel);
+            $response         = $sessionService->logout($sessionModelRead);
+            
+        } catch (\ModelNotFoundException $e) {
+            $response = $e->getMessage();
+        }
+        return $response;
+    }
+}
