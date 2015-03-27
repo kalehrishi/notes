9aeaf1f1a49d62965b59c4fb4cb790f6e2d763b1
amitheda <amit.heda@addonfactory.in>
2015-03-27 11:20:46 +0000

    Fixed checkstyle issue

--- app/View/View.php
+++ app/View/View.php
@@ -4,9 +4,8 @@ namespace Notes\View;
 
 class View
 {
-	public function render($fileName,$response=null)
-	{
-       require_once __dir__ ."/$fileName";
-         
+    public function render($fileName, $response = null)
+    {
+        require_once __dir__ . "/$fileName";
     }
 }
