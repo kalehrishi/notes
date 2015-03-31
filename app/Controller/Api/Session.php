a7f93a83578a3deeef9422998ead3815f56daa6a
gauribhapkar <gauribhapkar@github.com>
2015-03-31 10:18:03 +0000

    add Session Api Controller test cases

--- app/Controller/Api/Session.php
+++ app/Controller/Api/Session.php
@@ -1,11 +1,11 @@
 <?php
 namespace Notes\Controller\Api;
 
-use Notes\Service\Session as SessionService;
-use Notes\Model\User as UserModel;
-use Notes\Model\Session as SessionModel;
 use Notes\Response\Response as Response;
-use Notes\Model\Model as Model;
+use Notes\Model\Session as SessionModel;
+use Notes\Service\Session as SessionService;
+use Notes\Exception\ModelNotFoundException as ModelNotFoundException;
+use Notes\Model\User as UserModel;
 
 class Session
 {
@@ -16,28 +16,26 @@ class Session
     {
         $this->request = $request;
     }
-    
     public function post()
     {
         $data_array = $this->request->getData();
         $data       = $data_array['data'];
-        
         $userModel = new UserModel();
         $userModel->setEmail($data['email']);
         $userModel->setPassword($data['password']);
-        try {
-            $sessionService = new SessionService();
-            $response    = $sessionService->login($data);
-        } catch (\InvalidArgumentException $e) {
-            $this->message = $e->getMessage();
-        }
-        catch (\Exception $e) {
-            $this->message = $e->getMessage();
-        }
     
-        $objectResponse= new Response(200, $this->message, "1.0.0", $userModel->toArray());
-        return $objectResponse->getResponse();
+            $sessionService = new SessionService();
+            $response     = $sessionService->login($data);
+        if ($response) {
+             $objectResponse= new Response(200, $this->message, "1.0.0", $userModel->toArray());
+            return $objectResponse->getResponse();
+        } else {
+            $obj = new ModelNotFoundException();
+            $obj->setModel($sessionModel);
+            throw $obj;
+        }
     }
+    
     public function delete()
     {
         $data_array = $this->request->getData();
@@ -46,18 +44,21 @@ class Session
         $sessionModel = new sessionModel();
         $sessionModel->setAuthToken($data['authToken']);
         $sessionModel->setUserId($data['userId']);
-        try {
+    
             $sessionService   = new SessionService();
-            
             $sessionModelRead = $sessionService->isValid($sessionModel);
+            $response         = $sessionService->logout($sessionModelRead);
             
-            $response        = $sessionService->logout($sessionModelRead);
-            
-        } catch (\InvalidArgumentException $e) {
-            $this->message = $e->getMessage();
-        } catch (\Exception $e) {
-            $this->message = $e->getMessage();
+        if ($response) {
+            $objectResponse= new Response(200, $this->message, "1.0.0", $sessionModel->toArray());
+            return $objectResponse->getResponse();
+      
+        } else {
+            $obj = new ModelNotFoundException();
+            $obj->setModel($sessionModel);
+            throw $obj;
         }
+        
         $objectResponse= new Response(200, $this->message, "1.0.0", $sessionModel->toArray());
         return $objectResponse->getResponse();
     }
--- tests/Controller/Api/SessionTest.php
+++ tests/Controller/Api/SessionTest.php
@@ -1,14 +1,36 @@
 <?php
 namespace Notes\Controller\Api;
-
 use Notes\Controller\Api\Session as Session;
 use Notes\Request\Request as Request;
-//use Notes\Config\Config as Configuration;
+use Notes\Config\Config as Configuration;
 use Notes\Response\Response as Response;
 use Notes\Exception\ModelNotFoundException as ModelNotFoundException;
-
-class SessionTest extends \PHPUnit_Framework_TestCase
+class SessionTest extends \PHPUnit_Extensions_Database_TestCase
 {
+    private $connection;
+    
+    public function getConnection()
+    {
+        $config     = new Configuration("config.json");
+        $configData = $config->get();
+        $dbHost     = $configData['dbHost'];
+        $dbName     = $configData['dbName'];
+        $hostString = "mysql:host=$dbHost;dbname=$dbName";
+        try {
+            $this->connection = new \PDO($hostString, $configData['dbUser'], $configData['dbPassword']);
+            $this->connection->exec("set foreign_key_checks=0");
+            return $this->createDefaultDBConnection($this->connection, $dbName);
+        } catch (\PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+    
+    public function getDataSet()
+    {
+        return $this->createXMLDataSet(dirname(__FILE__) . '/_files/session_seed.xml');
+    }
+    
+    
     /**
      * @test
      *
@@ -29,8 +51,27 @@ class SessionTest extends \PHPUnit_Framework_TestCase
         
         $data = $sessionController->post();
         
-
-
+    }
+    
+    /**
+     * @test
+     * @expectedException         Notes\Exception\ModelNotFoundException
+     * @expectedExceptionMessage  Can Not Found Given Model In Database
+     */
+    public function it_should_throw_exception_with_invalid_email_password()
+    {
+        
+        $data    = '{
+                "data": {
+                            "email" : "abcd@gmail.com",
+                           "password" :"psd"
+                        }   
+                }';
+        $request = new Request();
+        $request->setData($data);
+        $sessionController = new Session($request);
+        
+        $response = $sessionController->post();
     }
     
     /**
@@ -42,7 +83,7 @@ class SessionTest extends \PHPUnit_Framework_TestCase
         $data    = '{
                 "data": {
                             "authToken" : "abc",
-                            "userId" :"1"
+                           "userId" :"1"
                         }   
                 }';
         $request = new Request();
@@ -51,5 +92,26 @@ class SessionTest extends \PHPUnit_Framework_TestCase
         
         $response = $sessionController->delete();
         
+    }
+    
+    /**
+     * @test
+     * @expectedException         Notes\Exception\ModelNotFoundException
+     * @expectedExceptionMessage  Can Not Found Given Model In Database
+     */
+    public function it_should_throw_exception_with_invalid_authToken_userId()
+    {
+        $data    = '{
+                "data": {
+                            "authToken" : "xyz",
+                           "userId" :"15"
+                        }   
+                }';
+        $request = new Request();
+        $request->setData($data);
+        
+        $sessionController = new Session($request);
+        
+        $response = $sessionController->delete();
     }
 }
