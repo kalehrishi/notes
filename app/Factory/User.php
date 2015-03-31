d19128e498b5b34c1533910c9750ae2893cc2796
amitheda <amit.heda@addonfactory.in>
2015-03-31 05:45:49 +0000

    Modified UserFactory and added testcases in UserFactoryTest

--- app/Factory/User.php
+++ app/Factory/User.php
@@ -2,10 +2,7 @@
 namespace Notes\Factory;
 
 use Notes\Model\User as UserModel;
-
 use Notes\Validator\InputValidator as InputValidator;
-
-use Notes\PasswordValidation\PasswordValidator as PasswordValidator;
 
 class User
 {
@@ -19,25 +16,26 @@ class User
     {
         
         $userModel = new UserModel();
-       
-        
-        if (!((isset($input['firstName']) && $this->validator->validString($input['firstName']))
+        if (((isset($input['firstName']) && $this->validator->validString($input['firstName']))
                 && (isset($input['lastName']) && $this->validator->validString($input['lastName']))
-                && (isset($input['email']) && $this->validator->validString($input['email']))
-                && (isset($input['password']) && $this->validator->isValidPassword($input['password']))
+                && (isset($input['email']) && $this->validator->notNull($input['email'])
+                && $this->validator->validEmail($input['email']))
+                && (isset($input['password']) && $this->validator->notNull($input['password'])
+                && $this->validator->isValidPassword($input['password']))
                 && (isset($input['createdOn'])))
-        
-           || ((isset($input['id']) && $this->validator->notNull($input['id']))
-            && (isset($input['firstName']) && $this->validator->validString($input['firstName']))
-            && (isset($input['lastName']) && $this->validator->validString($input['lastName']))
-            && (isset($input['email']) && $this->validator->validString($input['email']))
-            && (isset($input['password']) && $this->validator->isValidPassword($input['password']))
-            && (isset($input['createdOn'])) && (!isset($input['id'])))
-
-            || ((isset($input['id']) && $this->validator->notNull($input['id'])))
-
-            || ((isset($input['email']) && $this->validator->validString($input['email']))
-                && (isset($input['password']) && $this->validator->isValidPassword($input['password'])))) {
+            || ((isset($input['id']) && $this->validator->validNumber($input['id']))
+                && (isset($input['firstName']) && $this->validator->validString($input['firstName']))
+                && (isset($input['lastName']) && $this->validator->validString($input['lastName']))
+                && (isset($input['email']) && $this->validator->notNull($input['email'])
+                && $this->validator->validEmail($input['email'])) && (isset($input['password'])
+                && $this->validator->notNull($input['password'])
+                && $this->validator->isValidPassword($input['password']))
+                && (isset($input['createdOn'])) && (!isset($input['id'])))
+            || ((isset($input['id']) && $this->validator->validNumber($input['id'])))
+            || ((isset($input['email']) && $this->validator->notNull($input['email'])
+                && $this->validator->validEmail($input['email']))
+                && (isset($input['password']) && $this->validator->notNull($input['password'])
+                && $this->validator->isValidPassword($input['password'])))) {
             if (isset($input['id'])) {
                 $userModel->setId($input['id']);
                 
@@ -49,7 +47,7 @@ class User
             }
             if (isset($input['lastName'])) {
                 $userModel->setLastName($input['lastName']);
-               
+                
                 
             }
             if (isset($input['email'])) {
@@ -72,7 +70,7 @@ class User
         }
         
         return $userModel;
-      
+        
         
     }
 }
--- tests/Factory/UserTest.php
+++ tests/Factory/UserTest.php
@@ -5,10 +5,59 @@ use Notes\Factory\User as UserFactory;
 
 class UserTest extends \PHPUnit_Framework_TestCase
 {
+    /*
+     * @test
+     * @expectedException         InvalidArgumentException
+     * @expectedExceptionMessage  Input should be Number
+    */
+    public function it_should_throw_exception_when_id_is_not_number()
+    {
+        
+        $input       = array(
+            'id' => ' '
+            
+        );
+        $userFactory = new UserFactory();
+        $userModel   = $userFactory->create($input);
+        $this->assertEquals(1, $userModel->getId());
+        
+        
+    }
+    /*
+     * @test
+     * @expectedException         InvalidArgumentException
+     * @expectedExceptionMessage  Input should be Number
+    */
+    public function it_should_throw_exception_when_id_is_string()
+    {
+        
+        $input       = array(
+            'id' => 'test'
+            
+        );
+        $userFactory = new UserFactory();
+        $userModel   = $userFactory->create($input);
+    }
+    
+    /*
+     * @test
+     * @expectedException         InvalidArgumentException
+     * @expectedExceptionMessage  Input should not be null
+    */
+    public function it_should_throw_exception_when_id_is_null()
+    {
+        
+        $input       = array(
+            'id' => null
+            
+        );
+        $userFactory = new UserFactory();
+        $userModel   = $userFactory->create($input);
+        $this->assertEquals(1, $userModel->getId());
+    }
     /**
      * @test
-     *
-     */
+    */
     public function it_should_create_userModel_for_read()
     {
         
@@ -19,19 +68,14 @@ class UserTest extends \PHPUnit_Framework_TestCase
         $userFactory = new UserFactory();
         $userModel   = $userFactory->create($input);
         $this->assertEquals(1, $userModel->getId());
-        
-        
     }
-    
-    
     
     /**
      * @test
      *
-     */
+    */
     public function it_should_create_userModel_for_read_by_email_and_password()
     {
-        
         $input       = array(
             'email' => 'anusha@gmail.com',
             'password' => "anushA@h21"
@@ -40,16 +84,60 @@ class UserTest extends \PHPUnit_Framework_TestCase
         $userModel   = $userFactory->create($input);
         
         $this->assertEquals('anusha@gmail.com', $userModel->getEmail());
-        
         $this->assertEquals('anushA@h21', $userModel->getPassword());
+    }
+    
+    /**
+     * @test
+     * @expectedException         InvalidArgumentException
+     * @expectedExceptionMessage  Input should not be null
+    */
+    public function it_should_throw_exception_when_all_fields_are_null()
+    {
         
+        $input = array();
         
+        $userFactory = new UserFactory();
+        $userModel   = $userFactory->create($input);
+        
+    }
+    /**
+     * @test
+     * @expectedException          InvalidArgumentException
+     * @expectedExceptionMessage   Invalid Email
+    */
+    public function it_should_throw_exception_when_email_is_invalid()
+    {
+        
+        $input = array(
+            'email' => 'anushagmail.com',
+            'password' => "anushA@h21"
+        );
+        
+        $userFactory = new UserFactory();
+        $userModel   = $userFactory->create($input);
+    }
+    /**
+     * @test
+     * @expectedException          InvalidArgumentException
+     * @expectedExceptionMessage   Input should not be null
+    */
+    public function it_should_throw_exception_when_email_is_empty()
+    {
+        
+        $input = array(
+            'password' => "anushA@h21"
+        );
+        
+        $userFactory = new UserFactory();
+        $userModel   = $userFactory->create($input);
+        print_r($userModel);
     }
     
     /**
      * @test
      *
-     */
+    */
     public function it_should_create_userModel_for_create_user()
     {
         
@@ -69,15 +157,12 @@ class UserTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('priya@gmail.com', $userModel->getEmail());
         $this->assertEquals('sfhZ@223', $userModel->getPassword());
         $this->assertEquals('2014-10-29 20:59:59', $userModel->getCreatedOn());
-        
-        
     }
-    
     
     /**
      * @test
      *
-     */
+    */
     public function it_should_create_userModel_for_updating_user()
     {
         
@@ -98,6 +183,5 @@ class UserTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('priya@gmail.com', $userModel->getEmail());
         $this->assertEquals('sfhZ@223', $userModel->getPassword());
         $this->assertEquals('2014-10-29 20:59:59', $userModel->getCreatedOn());
-        
     }
 }
