d17cf482a9c52813fe6bdd17e5fd243639f7513c
amitheda <amit.heda@addonfactory.in>
2015-04-11 13:22:47 +0000

    Added behat.phar and one test

---
+++ ls-project/features/bootstrap/FeatureContext.php
@@ -0,0 +1,77 @@
+<?php
+
+use Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\TranslatedContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+//
+// Require 3rd-party libraries here:
+//
+//require_once 'PHPUnit/Autoload.php';
+//require_once 'PHPUnit/Framework/Assert/Functions.php';
+//
+
+/**
+ * Features context.
+ */
+class FeatureContext extends BehatContext
+{
+    /**
+     * Initializes context.
+     * Every scenario gets it's own context object.
+     *
+     * @param array $parameters context parameters (set them up through behat.yml)
+     */
+    public function __construct(array $parameters)
+    {
+        // Initialize your context here
+    }
+    /**
+     * @Given /^I am in a directory "([^"]*)"$/
+     */
+    public function iAmInADirectory($dir)
+    {
+        if (!file_exists($dir)) {
+            mkdir($dir);
+        }
+        chdir($dir);
+    }
+     /** @Given /^I have a file named "([^"]*)"$/ */
+    public function iHaveAFileNamed($file)
+    {   
+        touch($file);
+    }
+
+    /** @When /^I run "([^"]*)"$/ */
+    public function iRun($command)
+    {
+        exec($command, $output);
+        $this->output = trim(implode("\n", $output));
+    }
+
+    /** @Then /^I should get:$/ */
+    public function iShouldGet(PyStringNode $string)
+    {
+       // assertEquals($string->getRaw(), $this->output);
+        if ((string) $string !== $this->output) {
+            throw new Exception(
+                "Actual output is:\n" . $this->output
+            );
+        }
+    }
+
+//
+// Place your definition and hook methods here:
+//
+//    /**
+//     * @Given /^I have done something with "([^"]*)"$/
+//     */
+//    public function iHaveDoneSomethingWith($argument)
+//    {
+//        doSomethingWith($argument);
+//    }
+//
+}
