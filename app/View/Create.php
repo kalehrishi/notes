8ec41051308d92f19d5debaff1aef522891f4b78
pushpalata06 <pushpalata.marade@addonfactory.in>
2015-05-07 13:48:02 +0000

    show auto suggest tags using ajax and load view of create a Note

--- app/View/Create.php
+++ app/View/Create.php
@@ -2,118 +2,124 @@
 <html>
 <body>
 <head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script>
-$(document).ready(function() {
-  $(function() {
-        var scntDiv = $('#p_scents');
-        var i = $('#p_scents p').size() + 1;
-        
-        $('#addScnt').live('click', function() {
-                $('<p><label for="p_scnts"><input type="text" id="searchField" class="p_scnt" size="20" onkeyup="findTag(this.value)" name="tag" value="" autocomplete="off" placeholder="Enter Tag Name" /></label> <a href="#" id="remScnt">Remove</a></p>').appendTo(scntDiv);
-                i++;
-                return false;
-        });
-        
-        $('#remScnt').live('click', function() { 
-                if( i > 2 ) {
-                        $(this).parents('p').remove();
-                        i--;
-                }
-                return false;
-        });
-});
-});
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js">
 </script>
-<script type="text/javascript" src="../jquery.js"></script>
-<!-- <script type="text/javascript" src="../jquery.autocomplete.js"></script>
+<script type="text/javascript" src="http://code.jquery.com/ui/1.8.18/jquery-ui.min.js"></script>
 
- -->
-
+<link rel="stylesheet" type="text/css"
+href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.19/themes/cupertino/jquery-ui.css">
 <script>
-function findTag(str){
-  
-  console.log("Hello..");
-  
-  if (str.length==0) { 
-    document.getElementById("popups").innerHTML = "";
-    //document.getElementById("livesearch").innerHTML="";
-    //document.getElementById("livesearch").style.border="0px";
-    return;
-  }
-  if (window.XMLHttpRequest) {
-    // code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp=new XMLHttpRequest();
-  } else {  // code for IE6, IE5
-    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
+$(document).ready( function() {
+    
+        $.ajax({
+            type: 'GET',
+            url: '../complete.php',
+            data: 'id=testdata',
+            dataType: 'json',
+            cache: false,
+            success: function(result) {
+                console.log(result);
+                window.count = 0;
+    $("a[id^='add']").live("click", null, function () {
+        window.count++;
+        var txtTagsID= "txtTags" + window.count;
+        var btnAddID = "add" + window.count;
+        var btnDelID = "del" + window.count;
+        var div1 = $("#divTest");
+        var div2 = $("<div class='div-style'></div>")
+            .append($("<input id='" + txtTagsID+ "' />"
+                + "<a href='#' id='" + btnAddID + "' >Add</a> "
+                + "<a href='#' id='" + btnDelID + "' >Del</a>"));
+        div2.appendTo(div1);
+    });
 
-  xmlhttp.onreadystatechange=function() {
-    if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-      console.log(xmlhttp.responseText);
-      obj = JSON.parse(xmlhttp.responseText);
-      var tempDiv = document.createElement("div");
-        tempDiv.innerHTML = obj.tag;
-        tempDiv.onclick = makeChoice;
-        tempDiv.className = "suggestions";
-        document.getElementById("popups").appendChild(tempDiv);
-      //document.getElementById("livesearch").innerHTML=obj.tag;
-      //document.getElementById("livesearch").style.border="1px solid #A5ACB2";
+    $("a[id^='del']").live("click", null, function () {
+        if (window.count == 0) {
+            alert("You should have one textbox!");
+            return;
+        }
+        var li = $(this).parent();
+        li.remove();
+        window.count--;
+    });
+    
+    
+    
+    $("input:text[id^='txtTags']").live("focus.autocomplete", null, function () {
+        $(this).autocomplete({
+            source: result,
+            minLength: 0,
+            delay: 0
+        });
 
+        $(this).autocomplete("search");
+    });
     }
-  }
-  xmlhttp.open("GET","../complete.php?q="+str,true);
-  xmlhttp.send();
-
-
-  /*$(".p_scnt").autocomplete("../complete.php", {
-        selectFirst: true
-  });*/
-}
-function makeChoice(evt) {
-  var thisDiv = (evt) ? evt.target : window.event.srcElement;
-  document.getElementById("searchField").className.value = thisDiv.innerHTML;
-  //document.getElementsByClassName("p_scnt").value = thisDiv.innerHTML;
-  document.getElementById("popups").innerHTML = "";
-}
+ });
+    
+});
 </script>
-<link rel="stylesheet" type="text/css" href="../style.css">
+<style type="text/css">
+    .ul-style{
+    max-height:80px; 
+    width:350px; 
+    overflow-x:hidden; 
+    overflow-y:auto
+}
+.li-style{
+    font-size:medium;
+}
+button{
+    width:50px;
+    margin-left:5px;
+}
+input{
+    width:100px;
+}
 
+.ui-autocomplete {
+            max-height: 100px;
+            overflow-y: auto;
+            /* prevent horizontal scrollbar */
+            overflow-x: hidden;
+            /* add padding to account for vertical scrollbar */
+            padding-right: 20px;
+    }
+    /* IE 6 doesn't support max-height
+     * we use height instead, but this forces the menu to always be this tall
+     */
+    * html .ui-autocomplete {
+        height: 100px;
+    }
+</style>
+<link rel="stylesheet" type="text/css" href="../style.css">
 </head>
+<body>
 <form method="post">
 <div id="comment_form" class="container">
-<div>Create Note :</div>
-  <div>
-    <input type="hidden" name="id">
-    <input type="text" name="title" id="title" placeholder="Title">
-  </div>
-  <div>
-    <textarea rows="10" name="body" id="body" placeholder="Description"></textarea>
-  </div>
-  <?php 
-  $noteTags = array();
-  $userTag = array();
-  ?>
-    <input type="hidden" name="$noteTags['id']">
-    <input type="hidden" name="$noteTags['noteId']">
-    <input type="hidden" name="$noteTags['userTagId']">
-    <input type="hidden" name="$noteTags['isDeleted']">
-
-     
-<a href="#" id="addScnt">Add Another Tag</a>
-
-<div id="p_scents">
-    <p>
-        <label for="p_scnts"><input type="text" id="searchField" class="p_scnt" size="20" onkeyup="findTag(this.value)" name="tag" value="" autocomplete="off" placeholder="Enter Tag Name" />
-        </label>
-    </p>
-    <div id="popups"></div>
+    <div>Create Note :</div>
+    
+    <div>
+        <input type="hidden" name="id">
+        <input type="text" name="title" id="title" placeholder="Title">
+    </div>
+    
+    <div>
+        <textarea rows="10" name="body" id="body" placeholder="Description"></textarea>
+    </div>
+    
+    <div id="divTest">
+        <div class="div-style">
+            <input id="txtTags0" name = tags[]/>
+            <a href="#" id="add0">Add</a>
+            <a href="#" id="del0">Add</a>
+        </div>
+    </div>
+    <div><input type="submit" value="Save"></div>
+    
 </div>
-<input type="submit" value="Save">
-  </div>
-</form> 
+</form>
 </body>
-</html>
+<html>
