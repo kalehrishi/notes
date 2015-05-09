c3041cc05540b1b781c5b96a9420633c0e889df9
pushpalata06 <pushpalata.marade@addonfactory.in>
2015-05-09 13:16:35 +0000

    write controller logic for show auto suggest tags

--- app/Controller/Web/UserTag.php
+++ app/Controller/Web/UserTag.php
@@ -7,6 +7,12 @@ use Notes\Response\Response as Response;
 use Notes\Request\Request as Request;
 
 use Notes\Model\User as UserModel;
+use Notes\Collection\Collection as Collection;
+
+use Notes\Service\UserTag as UserTagService;
+use Notes\Service\Session as SessionService;
+use Notes\Model\Session as SessionModel;
+use Notes\Exception\ModelNotFoundException as ModelNotFoundException;
 
 class UserTag
 {
@@ -19,8 +25,31 @@ class UserTag
     }
     public function get()
     {
-        $test  = "test";
-        $json =  json_encode($test);
-        echo $json;
+       $sessionModel = new SessionModel();
+        $sessionModel->setUserId($this->request->getCookies()['userId']);
+        $sessionModel->setAuthToken($this->request->getCookies()['authToken']);
+        
+        $sessionService = new SessionService();
+        try {
+            $sessionService->isValid($sessionModel);
+        } catch (ModelNotFoundException $error) {
+            $app = \Slim\Slim::getInstance();
+            $app->redirect("/login");
+        }
+        $userId = $this->request->getCookies()['userId'];
+        $userModel = new UserModel();
+        $userModel->setId($userId);
+        $userTagService = new UserTagService();
+        try {
+            $userTagModel = $userTagService->get($userModel);
+            $toArray = $userTagModel->toArray();
+            $json =  json_encode($toArray);
+            echo $json;
+            
+        } catch (ModelNotFoundException $error) {
+            $response = $error->getMessage();
+            $this->view->render("Note.php", $response);
+        }
+
     }
 }
--- app/View/Create.php
+++ app/View/Create.php
@@ -10,6 +10,8 @@
 
 <link rel="stylesheet" type="text/css"
 href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.19/themes/cupertino/jquery-ui.css">
+
+<link rel="stylesheet" type="text/css" href="../CSS/style.css">
 <script>
 $(document).ready( function() {
     
@@ -20,7 +22,6 @@ $(document).ready( function() {
             datatype: 'json',
             cache: false,
             success: function(result) {
-                console.log("Hiiii");
                 console.log(result);
                 var obj = '{ "userTags" :'  + result +  '}';
                 console.log(obj);
@@ -102,8 +103,70 @@ input{
     * html .ui-autocomplete {
         height: 100px;
     }
+
+/*.container{
+  font-family: 'Lucida Grande', 'Helvetica Neue', sans-serif;
+  font-size: 13px;
+}
+
+#comment_form input, #comment_form textarea {
+  border: 4px solid rgba(0,0,0,0.1);
+  padding: 8px 10px;
+  font-weight: bold;
+  
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  border-radius: 5px;
+  
+  outline: 0;
+}
+
+#comment_form textarea {
+  width: 150px;
+  height: 100px;
+}
+
+#comment_form input[type="submit"] {
+  cursor: pointer;
+  background: -webkit-linear-gradient(top, #efefef, #ddd);
+  background: -moz-linear-gradient(top, #efefef, #ddd);
+  background: -ms-linear-gradient(top, #efefef, #ddd);
+  background: -o-linear-gradient(top, #efefef, #ddd);
+  background: linear-gradient(top, #efefef, #ddd);
+  color: #333;
+  text-shadow: 0px 1px 1px rgba(255,255,255,1);
+  border: 1px solid #ccc;
+}
+
+#comment_form input[type="submit"]:hover {
+  background: -webkit-linear-gradient(top, #eee, #ccc);
+  background: -moz-linear-gradient(top, #eee, #ccc);
+  background: -ms-linear-gradient(top, #eee, #ccc);
+  background: -o-linear-gradient(top, #eee, #ccc);
+  background: linear-gradient(top, #eee, #ccc);
+  border: 1px solid #bbb;
+}
+
+#comment_form input[type="submit"]:active {
+  background: -webkit-linear-gradient(top, #ddd, #aaa);
+  background: -moz-linear-gradient(top, #ddd, #aaa);
+  background: -ms-linear-gradient(top, #ddd, #aaa);
+  background: -o-linear-gradient(top, #ddd, #aaa);
+  background: linear-gradient(top, #ddd, #aaa); 
+  border: 1px solid #999;
+}
+
+#comment_form div {
+  margin-bottom: 8px;
+  margin: 10px;
+}
+.container {
+  width: 300px;
+  border: 1px solid black;
+
+}*/
+
 </style>
-<link rel="stylesheet" type="text/css" href="../style.css">
 </head>
 <body>
 <form method="post">
