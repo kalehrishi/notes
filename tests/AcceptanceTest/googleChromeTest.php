f7477c759760270efefc9b4efc1ec620b31fe167
amit heda <amit.heda@addonfactory.in>
2015-06-04 12:40:43 +0530

    Added acceptance test with chrome-browser

---
+++ tests/AcceptanceTest/googleChromeTest.php
@@ -0,0 +1,31 @@
+<?php
+
+class googleTest extends PHPUnit_Extensions_Selenium2TestCase
+{
+    protected function setUp()
+    {
+        $this->setHost('localhost');
+        $this->setBrowser('chromium-browser');
+        $this->setBrowserUrl('http://localhost/');
+    }
+
+    public function testTitle()
+    {
+        $this->url('index.php/login');
+        $this->assertEquals('Login', $this->title());
+
+        $this->byName('email')->value("gau@bhapkar.com");
+        $this->byName('password')->value("Gauri@12");
+        $this->byCssSelector('form')->submit();
+        $welcom=$this->byCssSelector('div');
+        $this->assertEquals('Login', $this->title());
+        //$this->assertEquals('Html', $welcom);
+        
+    }
+    public function testHomeTitle()
+    {
+        $this->url('index.php/');
+        $this->assertEquals('Home', $this->title());
+        
+    }
+}
