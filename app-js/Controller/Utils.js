f1403f5cd73a14edab26ee1788b395f162891816
pushpalata06 <pushpalata.marade@addonfactory.in>
2015-07-21 13:45:49 +0000

    Remove setRequestHearders

--- app-js/Controller/Utils.js
+++ app-js/Controller/Utils.js
@@ -4,11 +4,6 @@ Notes.utils = {
         
         xhr.open("POST", url, isAsync);
         xhr.setRequestHeader("Content-Type", "application/json");
-        
-        xhr.setRequestHeader("Access-Control-Allow-Credentials", "true");
-        xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-        xhr.setRequestHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT");
-        xhr.setRequestHeader("Access-Control-Allow-Headers", "HTTP_X_REQUESTED_WITH, Content-Type, Accept, Authorization");
         xhr.send(data);
 
         xhr.onreadystatechange = function () {
