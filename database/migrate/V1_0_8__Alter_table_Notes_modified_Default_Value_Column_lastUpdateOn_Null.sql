804b89231c90559a5dceb69803cccb8b1ed59fec
amitheda <amit.heda@addonfactory.in>
2015-08-05 13:21:24 +0000

    Added apache ivy code to publish artifact on nexus

--- database/migrate/V1_0_8__Alter_table_Notes_modified_Default_Value_Column_lastUpdateOn_Null.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE Notes modify lastUpdateOn TIMESTAMP DEFAULT NULL;
--- tests/php/unit/Response/_files/json.js
+++
@@ -1,1 +0,0 @@
-{"status":"200","message":"OK","version":"1.0.0","data":{"firstName":"Joy","lastName":"Mock","0":{"tag":"Politics"}},"debugData":null}
