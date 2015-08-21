72b8bfd673b61da067a4016bc442eee575596e5a
pushpalata06 <pushpalata.marade@addonfactory.in>
2015-08-21 13:33:59 +0000

    Remove path of mustache templates

--- app/View/Login.php
+++ app/View/Login.php
@@ -4,23 +4,19 @@ $data = json_decode($response, true);
 $m = new Mustache_Engine;
 Mustache_Autoloader::register();
 
-$m = new Mustache_Engine(array(
-    'loader' => new Mustache_Loader_FilesystemLoader('/vagrant/template/'),
-));
-
-
 //Add Header
 $title = array('title' => "Login");
-echo $m->render('header.mustache', $title);
+$m->render('header.mustache', $title);
 
 // Add middle container
-echo $m->render('template.mustache');
+$m->render('template.mustache');
 if (is_string($data['data'])) {
-		echo "Error :-<p class='error'>" . $data['data'] ."</p>";
+		?> <p class='error'><?php echo $data['data']; ?></p>
+<?php
    } 
 
 //Add footer
 $footer = array();
-echo $m->render('footer.mustache', $footer);
+$m->render('footer.mustache', $footer);
 ?>
 
