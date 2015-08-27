94ef3d871801e35b58e0dd37f1fefe9849b5a46b
amitheda <amit.heda@addonfactory.in>
2015-08-27 10:02:40 +0000

    Added Acceptance test

---
+++ tests/php/acceptance/LoginTest.php
@@ -0,0 +1,24 @@
+<?php
+
+class LoginTest extends PHPUnit_Extensions_Selenium2TestCase
+{
+    protected function setUp()
+    {
+        $this->setHost('localhost');
+        $this->setBrowser('firefox');
+        $this->setBrowserUrl(getenv('URL_ADDRESS'));
+    }
+    /**
+    * @large
+    **/
+    public function testTitle()
+    {
+        $this->url('/index.php/login');
+        $this->assertEquals('Login', $this->title());
+        $this->byName('email')->value("gau@bhapkar.com");
+        $this->byName('password')->value("Gauri@12");
+        $this->byCssSelector('form')->submit();
+        $welcom=$this->byCssSelector('div');
+        $this->assertEquals('Login', $this->title());       
+    }
+}
