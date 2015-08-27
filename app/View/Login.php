933e2f26000685ac5c527b9590ea1b54fab020c5
pushpalata06 <pushpalata.marade@addonfactory.in>
2015-08-27 12:17:00 +0000

    Remove mustache code

--- app/View/Login.php
+++ app/View/Login.php
@@ -1,22 +1,57 @@
 <?php
 $data = json_decode($response, true);
-
-$m = new Mustache_Engine;
-Mustache_Autoloader::register();
-
-//Add Header
-$title = array('title' => "Login");
-$m->render('header.mustache', $title);
-
-// Add middle container
-$m->render('template.mustache');
-if (is_string($data['data'])) {
-		?> <p class='error'><?php echo $data['data']; ?></p>
-<?php
-   } 
-
-//Add footer
-$footer = array();
-$m->render('footer.mustache', $footer);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  <style>
+ .error { 
+  }
+</style>
+
+<script type="text/javascript" src="../../lib/jquery/dist/jquery.min.js"></script>
+
+<script type="text/javascript" src="../../lib/main-min.js"></script>
+
+</head>
+
+<body>
+<div id="container">
+
+    <table>
+      <tr>
+        <td>Email:</td>
+        <td><input type="email" id="email" required/></td>
+      </tr>
+      <tr> 
+        <td>Password:</td>
+        <td><input type="password" id="password" required="required"/></td> 
+      </tr>
+      <tr><td>
+      <input type="hidden" id="id"/>
+      <input type="hidden" id="firstName"/>
+      <input type="hidden" id="lastName"/></td>
+      </tr>
+      <tr>
+        <td><input type="hidden" id="createdOn" /></td>
+        <td><input type="hidden" id="userModel" /></td>
+      </tr>
+      <tr><?php
+        if (is_string($data['data'])) {
+            ?>
+            <td>Error :-  </td>
+            <td><p class="error" ><?php echo $data['data'];
+        }?></p></td>
+       </tr>
+      <tr>
+        <td><input type="submit" value="Login" id="login"/></td>
+        <td><input type="reset"  value="Reset" id="reset"/></td>
+      </tr>
+      </table>
+    </div>
+    <div id="errorMessage"></div>
+</body>
+</html>
 
