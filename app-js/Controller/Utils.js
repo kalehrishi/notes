522cccd4180cb8109bb5e0178ae8b446c92e94c3
pushpalata06 <pushpalata.marade@addonfactory.in>
2015-10-15 12:35:14 +0000

    added GET request method

--- app-js/Controller/Utils.js
+++ app-js/Controller/Utils.js
@@ -19,5 +19,27 @@ Notes.utils = {
                 }
             }
         };
+    },
+
+    get: function (url, isAsync, onSuccess, onFailure) {
+        var xhr = new XMLHttpRequest();
+        
+        xhr.open("GET", url, isAsync);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send();
+
+        xhr.onreadystatechange = function () {
+
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                console.log("XHR====",xhr);
+                var response = JSON.parse(xhr.responseText);
+                console.log("OnXhr Success Response: ", response);
+                if (response.status === 0) {
+                    onFailure(response);
+                } else {
+                    onSuccess(response);
+                }
+            }
+        };
     }
 };
